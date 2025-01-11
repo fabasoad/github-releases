20bd777e5d300bea518f20d1645b2c6b2d260bc9
fabasoad <fabasoad@gmail.com>
2025-01-12 01:13:56 +0900

    Rebuild action/index.js

--- src/index.ts
+++ src/index.ts
@@ -83,6 +83,7 @@ interface SmallRelease {
                     new Date(a.published_at as string).getTime())
         );
 
+        core.info(`Limit: ${inputs.limit} (${typeof inputs.limit})`)
         let releases: SmallRelease[] = releaseList
             .slice(0, inputs.limit == 0 ? releaseList.length : inputs.limit)
             .map(x => ({
