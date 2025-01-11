6687f2be586865c339d9fe1e864a8fddad874f4a
fabasoad <fabasoad@gmail.com>
2025-01-12 00:51:23 +0900

    Test 2

--- src/index.ts
+++ src/index.ts
@@ -83,7 +83,7 @@ interface SmallRelease {
                     new Date(a.published_at as string).getTime())
         );
 
-        console.log(`Limit: ${inputs.limit}`)
+        core.info(`Limit: ${inputs.limit}`)
         let releases: SmallRelease[] = releaseList
             .slice(0, inputs.limit == 0 ? releaseList.length : inputs.limit)
             .map(x => ({
